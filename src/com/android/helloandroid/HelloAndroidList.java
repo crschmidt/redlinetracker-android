d9d4f33b8b6306deb74a97d701e7769ab81d23d8
Christopher Schmidt <crschmidt@crschmidt.net>
2011-10-18 00:37:45 -0400

    Working on getting ListActivity working.

--- src/com/android/helloandroid/HelloAndroidList.java
+++ src/com/android/helloandroid/HelloAndroidList.java
@@ -1,14 +1,18 @@
 package com.android.helloandroid;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import java.util.HashMap;
+import java.util.List;
+import android.widget.SimpleAdapter;
+import android.widget.ArrayAdapter;
 
 
-public class HelloAndroidList extends Activity
+public class HelloAndroidList extends ListActivity
 {
     /** Called when the activity is first created. */
     @Override
@@ -16,9 +20,12 @@ public class HelloAndroidList extends Activity
     {
         super.onCreate(savedInstanceState);
         String dir = getIntent().getStringExtra(HelloAndroid.direction);
-//        setContentView(R.layout.list);
-        TextView tv = new TextView(this);
-        tv.setText(dir);
-        setContentView(tv);
+        if (dir.equals("North")) {
+            HashMap north = new HashMap();
+            north.put("RCENN", "Central Station");
+            String[] names = new String[] { "Central Station" };
+            setListAdapter(new ArrayAdapter<String>(this,
+                            android.R.layout.simple_list_item_1, names));
+        }
     }
 }
